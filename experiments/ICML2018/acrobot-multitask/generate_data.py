43fe969a5e5df659f9f083e7ccf3c8f7c13a0c3e
AndreaTirinzoni <andrea.tirinzoni@polimi.it>
2018-01-13 10:56:01 +0100

    Added data for acrobot-multitask

--- experiments/ICML2018/acrobot-multitask/generate_data.py
+++ experiments/ICML2018/acrobot-multitask/generate_data.py
@@ -1,18 +1,17 @@
 from trlib.utilities.wfqi_utils import generate_source
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel, ExpSineSquared
 import numpy as np
-from trlib.policies.policy import Uniform
 from trlib.utilities.data import load_object
 from trlib.utilities.evaluation import evaluate_policy
-from trlib.environments.acrobot_hybrid import AcrobotHybrid
 from acro_policy import AcrobotPolicy
+from trlib.environments.acrobot_multitask import AcrobotMultitask
 
-source_mdp_1 = AcrobotHybrid(m1 = 0.8, m2 = 0.8, l1 = 0.8, l2 = 0.8)
-source_mdp_2 = AcrobotHybrid(m1 = 0.8, m2 = 1.3, l1 = 0.9, l2 = 1.2)
-source_mdp_3 = AcrobotHybrid(m1 = 0.9, m2 = 0.6, l1 = 1.1, l2 = 0.7)
+source_mdp_1 = AcrobotMultitask(m1 = 0.9, m2 = 0.6, l1 = 1.1, l2 = 0.7, task = "swing-up")
+source_mdp_2 = AcrobotMultitask(l1 = 0.95, l2 = 0.95, m1 = 0.95, m2 = 1.0, task = "rotate")
 
-mdp = source_mdp_3
-file_name = "source_data_3_200"
+mdp = source_mdp_2
+file_name = "source_data_2"
+policy_file = "source_policy_2"
 
 k1 = ConstantKernel(2.74**2, constant_value_bounds="fixed") * RBF(length_scale=1.51, length_scale_bounds="fixed")
 k2 = ConstantKernel(2.14**2, constant_value_bounds="fixed") * RBF(length_scale=0.92, length_scale_bounds="fixed")
@@ -20,6 +19,8 @@ k3 = ConstantKernel(2.42**2, constant_value_bounds="fixed") * RBF(length_scale=2
 k4 = ConstantKernel(3.14**2, constant_value_bounds="fixed") * RBF(length_scale=2.76, length_scale_bounds="fixed")
 kernel_st = [k1,k2,k3,k4]
 
-generate_source(mdp, n_episodes = 200, test_fraction = 0, file_name = file_name, policy = AcrobotPolicy(0.1),
-                policy_file_name = None, kernel_rw = None, kernel_st = None, 
-                load_data = False, fit_rw = False, fit_st = True, subtract_noise_rw=False, subtract_noise_st=False)
+kernel_rw = ConstantKernel(2.03**2, constant_value_bounds="fixed")  * RBF(length_scale=2.57, length_scale_bounds="fixed")
+
+generate_source(mdp, n_episodes = 50, test_fraction = 0, file_name = file_name, policy = None,
+                policy_file_name = policy_file, kernel_rw = None, kernel_st = None, 
+                load_data = False, fit_rw = True, fit_st = True, subtract_noise_rw=False, subtract_noise_st=False)
