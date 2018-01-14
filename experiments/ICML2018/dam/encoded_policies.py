9475411370c131fff0306d401fdf2bc22f23ac98
AndreaTirinzoni <andrea.tirinzoni@polimi.it>
2018-01-14 18:46:11 +0100

    Added Dam policies

--- experiments/ICML2018/dam/encoded_policies.py
+++ experiments/ICML2018/dam/encoded_policies.py
@@ -1,73 +1,96 @@
 from trlib.policies.policy import Policy
+import numpy as np
 
-class EncodedPolicy(Policy):
+class DamPolicy(Policy):
+
+    def __init__(self, epsilon = 0):
+        self._epsilon = epsilon
+        self._actions = [0, 3, 5, 7, 10, 15, 20, 30]
+        
+class DamPolicyT(DamPolicy):
     
     def sample_action(self, state):
+        
+        if np.random.uniform() < self._epsilon:
+            return self._actions[np.random.randint(8)]
+        
         day = state[1]
         
         if day < 60:
-            return 5
+            return 15.0
         elif day < 120:
-            return 4
+            return 10.0
         elif day < 180:
-            return 4
+            return 7.0
         elif day < 240:
-            return 3
+            return 7.0
         elif day < 300:
-            return 4
+            return 10.0
         else:
-            return 5
+            return 15.0
         
-class EncodedPolicy1(Policy):
+class DamPolicyS1(DamPolicy):
     
     def sample_action(self, state):
+        
+        if np.random.uniform() < self._epsilon:
+            return self._actions[np.random.randint(8)]
+        
         day = state[1]
         
         if day < 60:
-            return 3
+            return 10
         elif day < 120:
-            return 3
+            return 10
         elif day < 180:
-            return 6
+            return 30
         elif day < 240:
-            return 6
+            return 20
         elif day < 300:
-            return 2
+            return 7
         else:
-            return 1
+            return 5
         
-class EncodedPolicy2(Policy):
+class DamPolicyS2(DamPolicy):
     
     def sample_action(self, state):
+        
+        if np.random.uniform() < self._epsilon:
+            return self._actions[np.random.randint(8)]
+        
         day = state[1]
         
         if day < 60:
-            return 6
+            return 20
         elif day < 120:
-            return 5
+            return 20
         elif day < 180:
-            return 2
+            return 7
         elif day < 240:
-            return 3
+            return 7
         elif day < 300:
-            return 3
+            return 7
         else:
-            return 3
+            return 10
         
-class EncodedPolicy3(Policy):
+class DamPolicyS3(DamPolicy):
     
     def sample_action(self, state):
+        
+        if np.random.uniform() < self._epsilon:
+            return self._actions[np.random.randint(8)]
+        
         day = state[1]
         
         if day < 60:
-            return 5
+            return 15
         elif day < 120:
-            return 4
+            return 10
         elif day < 180:
-            return 2
-        elif day < 240:
-            return 3
-        elif day < 300:
-            return 4
-        else:
             return 5
+        elif day < 240:
+            return 7
+        elif day < 300:
+            return 10
+        else:
+            return 15
